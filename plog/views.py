950527bd2e0b43c511e8f06c7019cf575c7f76a0
Cameron Rodda <cameron.rodda@synchrotron.org.au>
2016-07-05 19:19:01 +1000

    starting to flesh out the pv post interface

--- plog/views.py
+++ plog/views.py
@@ -27,7 +27,26 @@ def rfid():
     db.session.add(tmp)
     db.session.commit()
 
+    print("Received data: %s : %s : %s" % ( timestamp, device, cardID ))
+
     return cardID
+
+@app.route('/pv', methods=['POST'])
+def pv():
+    timestamp = request.form['timestamp']
+    pv = request.form['pv']
+    pv_val = request.form['pv_val']
+    device = request.form['source']
+
+    mod_time = timestamp.split(" ")[1]
+    mod_date = timestamp.split(" ")[0]
+
+
+    return pv
+
+@app.route('/ui', methods=['POST'])
+def ui():
+    pass
 
 @app.errorhandler(404)
 def page_not_found(e):
