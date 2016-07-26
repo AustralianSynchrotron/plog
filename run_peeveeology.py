78c007875ff06407a3c73c5d9340dbd1c8700f0b
Cameron Rodda <cameron.rodda@synchrotron.org.au>
2016-07-26 17:16:56 +1000

    replaced returned number value with string value/ fixed the callbacks -passing custom variable

--- run_peeveeology.py
+++ run_peeveeology.py
@@ -2,6 +2,7 @@ import requests
 import datetime
 import time
 import epics
+from functools import partial
 
 from plog.config import rfid_host, rfid_port
 
@@ -9,6 +10,10 @@ DEVICE = 'PV'  # for interfaces CR,SW,NE,BS,BN,PV,UI
 DEBUG = True
 PV_SERVER = rfid_host + ":" + rfid_port
 INIT = True
+DOOR_STATE = ['NaN','Open','Closed']
+SHUTTER_STATE = ['NaN','Closed','Enabled']
+ACCESS_STATE = ['NaN','No Access','Authorised','Open','Error']
+KEY_STATE = ['NaN','Removed','Present']
 
 now = datetime.datetime.now()
 fnow = now.strftime("%Y-%m-%d %H:%M:%S")
@@ -16,17 +21,18 @@ print("%s> Available PV's..." % fnow )
 print("Connected to PV's: " )
 
 
-def onChange(pvname=None, value=None, char_value=None, timestamp=None, **kw):
+def onChange(pvname=None, value=None, char_value=None, timestamp=None, enum_list=None, **kw):
     # output data to command line
     now = datetime.datetime.now()
     fnow = now.strftime("%Y-%m-%d %H:%M:%S")
+    # print("%s> Data: (%s) (%s) (%s) (%s)" % (fnow, time.ctime(timestamp), pvname, value, enum_list[value]))
     if DEBUG:
-        print("%s> Data: (%s) (%s) (%s)" % ( fnow, time.ctime(timestamp), pvname, char_value ))
+        print("%s> Data: (%s) (%s) (%s) (%s)" % ( fnow, time.ctime(timestamp), pvname, value, enum_list[value] ))
 
     #send data to server
     try:
         if not INIT:
-            r = requests.post(PV_SERVER + "/pv/", data={'timestamp': fnow, 'pv': pvname, 'pv_val': char_value, 'source': DEVICE})
+            r = requests.post(PV_SERVER + "/pv/", data={'timestamp': fnow, 'pv': pvname, 'pv_val': enum_list[value], 'source': DEVICE})
             print("%s> %s %s" % (fnow, r.status_code, r.reason))
     except requests.exceptions.RequestException as e:
         now = datetime.datetime.now()
@@ -34,25 +40,33 @@ def onChange(pvname=None, value=None, char_value=None, timestamp=None, **kw):
         print("%s> Error: {}".format(e) % fnow)
 
 
-master_shutter = epics.PV("PP00:MASTER_SHUTTER_ENABLE_STATUS", callback=onChange) # Master Shutter status
-br_state = epics.PV("PP00:LINAC_BR_ACCESS_MODE_STATUS",callback=onChange) # BR Status
-sr_state = epics.PV("PP00:SR_ACCESS_MODE_STATUS",callback=onChange) # SR Status
-sr_sw_key = epics.PV("PP00:SR_SW_LAB_KEY_STATUS",callback=onChange) # SR SW Labrynth Door Keys State
-sr_sw_door = epics.PV("PP00:SR_SW_LAB_DOOR_STATUS",callback=onChange) # SR SW Labrynth Door State
-sr_ne_key = epics.PV("PP00:SR_NE_LAB_KEY_STATUS",callback=onChange) # SR NE Labrynth Door Keys State
-sr_ne_door = epics.PV("PP00:SR_NE_LAB_DOOR_STATUS",callback=onChange) # SR NE Labrynth Door State
-br_sth_key = epics.PV("PP00:BR_LAB_SOUTH_KEY_STATUS",callback=onChange) # Booster Sth Labrynth Door Keys State
-br_sth_door = epics.PV("PP00:BR_LAB_SOUTH_DOOR_STATUS",callback=onChange) # Booster Sth Labrynth Door State
-br_nth_key = epics.PV("PP00:BR_LAB_NORTH_KEY_STATUS",callback=onChange) # Booster Nth Labrynth Door Keys State
-br_nth_door = epics.PV("PP00:BR_LAB_NORTH_DOOR_STATUS",callback=onChange) # Booster Nth Labrynth Door State
-linac_key = epics.PV("PP00:LINAC_LAB_KEY_STATUS",callback=onChange) # Linac Labrynth Door Keys State
-lniac_door = epics.PV("PP00:LINAC_LAB_DOOR_STATUS",callback=onChange) # Linac Labrynth Door State
-zone1 = epics.PV("PP00:SEARCH_ZONE1_STATUS",callback=onChange) # Zone 1 Search State
-zone2 = epics.PV("PP00:SEARCH_ZONE2_STATUS",callback=onChange) # Zone 2 Search State
-zone3 = epics.PV("PP00:SEARCH_ZONE3_STATUS",callback=onChange) # Zone 3 Search State
-zone4 = epics.PV("PP00:SEARCH_ZONE4_STATUS",callback=onChange) # Zone 4 Search State
-zone5 = epics.PV("PP00:SEARCH_ZONE5_STATUS",callback=onChange) # Zone 5 Search State
-zone6 = epics.PV("PP00:SEARCH_ZONE6_STATUS",callback=onChange) # Zone 6 Search State
+master_shutter = epics.PV("PP00:MASTER_SHUTTER_ENABLE_STATUS") # Master Shutter status
+master_shutter.add_callback(callback=onChange,enum_list=SHUTTER_STATE)
+br_state = epics.PV("PP00:LINAC_BR_ACCESS_MODE_STATUS") # BR Status
+br_state.add_callback(callback=onChange,enum_list=SHUTTER_STATE)
+sr_state = epics.PV("PP00:SR_ACCESS_MODE_STATUS") # SR Status
+sr_state.add_callback(callback=onChange,enum_list=SHUTTER_STATE)
+sr_sw_key = epics.PV("PP00:SR_SW_LAB_KEY_STATUS") # SR SW Labrynth Door Keys State
+sr_sw_key.add_callback(callback=onChange,enum_list=KEY_STATE)
+sr_sw_door = epics.PV("PP00:SR_SW_LAB_DOOR_STATUS") # SR SW Labrynth Door State
+sr_sw_door.add_callback(callback=onChange,enum_list=DOOR_STATE)
+sr_ne_key = epics.PV("PP00:SR_NE_LAB_KEY_STATUS") # SR NE Labrynth Door Keys State
+sr_ne_key.add_callback(callback=onChange,enum_list=KEY_STATE)
+sr_ne_door = epics.PV("PP00:SR_NE_LAB_DOOR_STATUS") # SR NE Labrynth Door State
+sr_ne_door.add_callback(callback=onChange,enum_list=DOOR_STATE)
+br_sth_key = epics.PV("PP00:BR_LAB_SOUTH_KEY_STATUS") # Booster Sth Labrynth Door Keys State
+br_sth_key.add_callback(callback=onChange,enum_list=KEY_STATE)
+br_sth_door = epics.PV("PP00:BR_LAB_SOUTH_DOOR_STATUS") # Booster Sth Labrynth Door State
+br_sth_door.add_callback(callback=onChange,enum_list=DOOR_STATE)
+br_nth_key = epics.PV("PP00:BR_LAB_NORTH_KEY_STATUS") # Booster Nth Labrynth Door Keys State
+br_nth_key.add_callback(callback=onChange,enum_list=KEY_STATE)
+br_nth_door = epics.PV("PP00:BR_LAB_NORTH_DOOR_STATUS") # Booster Nth Labrynth Door State
+br_nth_door.add_callback(callback=onChange,enum_list=DOOR_STATE)
+linac_key = epics.PV("PP00:LINAC_LAB_KEY_STATUS") # Linac Labrynth Door Keys State
+linac_key.add_callback(callback=onChange,enum_list=KEY_STATE)
+linac_door = epics.PV("PP00:LINAC_LAB_DOOR_STATUS") # Linac Labrynth Door State
+linac_door.add_callback(callback=onChange,enum_list=DOOR_STATE)
+
 
 print("Waiting for PV Data Callbacks to Connect...")
 time.sleep(10)
